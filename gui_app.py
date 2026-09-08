a2ca33524300feebdb0b561cd4de2dfb3f681e50
vanitoo <56897888+vanitoo@users.noreply.github.com>
2026-09-08 15:40:26 +0300

    Switch GUI entrypoint to v5 recommendations

--- gui_app.py
+++ gui_app.py
@@ -2,7 +2,7 @@ from streamlit_compat import install_streamlit_width_compat
 
 install_streamlit_width_compat()
 
-from gui_app_v4 import main
+from gui_app_v5 import main
 
 
 if __name__ == "__main__":
